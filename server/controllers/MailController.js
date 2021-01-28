1d7734e22de530426aae365ca9c5cc2131db6ee9
TommySus <tommysusanto77@gmail.com>
2021-01-28 14:24:06 +0700

    done

--- server/controllers/MailController.js
+++ server/controllers/MailController.js
@@ -11,12 +11,27 @@ class MailController {
             UserId: req.loggedInUser.id
         }
         Mail.create(obj)
-        .then(data => {
-            let month = data.createdAt.toISOString().slice(5,7)
-            let day =  data.createdAt.toISOString().slice(8,10)
-            let year = data.createdAt.toISOString().slice(0,4)
-            res.status(201).json({
-                nomorSurat: `${data.jenisSurat}/${data.id}/${data.instansi}/PKP/${day}.${month}/${year}`
+        .then(data1 => {
+            let month = data1.createdAt.toISOString().slice(5,7)
+            let day =  data1.createdAt.toISOString().slice(8,10)
+            let year = data1.createdAt.toISOString().slice(0,4)
+            return Mail.findOne({where: {id: data1.id}, include: User})
+            .then(data => {
+                res.status(201).json({
+                    id: data.id,
+                    kepada: data.kepada,
+                    perihal: data.perihal,
+                    instansi: data.instansi,
+                    jenisSurat: data.jenisSurat,
+                    namaPenanggungJawab: data.User.name,
+                    instansiPenanggungJawab: data.User.divisi,
+                    emailPenanggungJawab: data.User.mail,
+                    tanggalDiBuat: data.createdAt.toISOString().slice(0,10),
+                    nomorSurat: `${data.jenisSurat}/${data.id}/${data.instansi}/PKP/${day}.${month}/${year}`
+                })
+            })
+            .catch(error => {
+                next(error)
             })
         })
         .catch(error => {
@@ -31,9 +46,9 @@ class MailController {
                 id: data.id,
                 kepada: data.kepada,
                 perihal: data.perihal,
-                instansi: data.instansi,
                 jenisSurat: data.jenisSurat,
                 namaPenanggungJawab: data.User.name,
+                instansiPenanggungJawab: data.User.divisi,
                 emailPenanggungJawab: data.User.mail,
                 tanggalDiBuat: data.createdAt.toISOString().slice(0,10)
             })
